b2a9317c9c980f20eee90e4e3c801b4dae79c2f0
Sumit <ghewdesumit@gmail.com>
2020-05-07 11:29:11 -0400

    saving the created user to the db and checking if the user exists, encrypting the password with bcrypt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // @route  POST api/users
 // @desc   Register User
@@ -14,7 +19,7 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res, next) => {
+  async (req, res, next) => {
     /** req.body holds the object which is sent
      * from the POST request and using middleware
      * it allows it to use the object here directly
@@ -23,6 +28,39 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      const avatar = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User Registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+
     console.log(req.body);
     return res.send('User Route');
   }
