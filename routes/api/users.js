17d0da169ea50e54043fe4983cbf441d5f62e6c9
Sumit <ghewdesumit@gmail.com>
2020-05-07 12:17:46 -0400

    sending the jwt token to client

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 
 const { check, validationResult } = require('express-validator');
 
@@ -55,14 +57,27 @@ router.post(
 
       await user.save();
 
-      res.send('User Registered');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      // Change expires 3600
+      jwt.sign(
+        payload,
+        config.get('jwtToken'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          console.log('sending the token', token);
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
     }
-
-    console.log(req.body);
-    return res.send('User Route');
   }
 );
 
