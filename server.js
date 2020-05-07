bb947c76512467a3e4cf995b6b08ca06ca669784
Sumit <ghewdesumit@gmail.com>
2020-05-06 23:37:45 -0400

    connected to mongoDB and created the routes for the auth, post, user and profile

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 
+const connectDB = require('./config/db');
+
+//connect mongo DB
+connectDB();
+
 app.get('/', (req, res) => res.send('API running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
 
 const PORT = process.env.PORT || 5000;
 
