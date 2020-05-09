c923d6a434492b41d70d4762d3dc319ad6c166f7
Sumit <ghewdesumit@gmail.com>
2020-05-09 15:15:05 -0400

    added put for like and unlike

--- routes/api/post.js
+++ routes/api/post.js
@@ -111,4 +111,102 @@ router.post(
   }
 );
 
+// @route  POST api/post
+// @desc   Posting a post
+// @access Private
+router.post(
+  '/',
+  [auth, check('text', 'Text is required').not().isEmpty()],
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: error.away() });
+    }
+    try {
+      const isValidObject = mongoose.Types.ObjectId.isValid(req.user.id);
+
+      if (!isValidObject)
+        return res.status(404).json({ msg: 'The Post id is wrong' });
+
+      const user = await User.findById(req.user.id).select('-password');
+      let newPost = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+      newPost = new Post(newPost);
+      await newPost.save();
+      res.json(newPost);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error for posting post');
+    }
+  }
+);
+
+// @route  PUT api/post/like/:id
+// @desc   Like a post
+// @access Private
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const isValidObject = mongoose.Types.ObjectId.isValid(req.params.id);
+
+    if (!isValidObject)
+      return res.status(404).json({ msg: 'The Post id is wrong' });
+
+    const post = await Post.findById(req.params.id);
+
+    if (
+      post.likes.filter((item) => item.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: 'Post already liked' });
+    }
+
+    post.likes.push({ user: req.user.id });
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error for liking the post');
+  }
+});
+
+// @route  PUT api/post/unlike/:id
+// @desc   unLike a post
+// @access Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const isValidObject = mongoose.Types.ObjectId.isValid(req.params.id);
+
+    if (!isValidObject)
+      return res.status(404).json({ msg: 'The Post id is wrong' });
+
+    let post = await Post.findById(req.params.id);
+
+    let postUser = post.likes.filter(
+      (item) => item.user.toString() === req.user.id
+    );
+
+    if (postUser.length == 0) {
+      return res.status(400).json({ msg: 'Already unliked or doesnt exist' });
+    }
+
+    const removeIndex = post.likes
+      .map((item) => item.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error for unliking the post');
+  }
+});
+
 module.exports = router;
